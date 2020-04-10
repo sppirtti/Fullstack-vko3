78e461047a430b91ff24c4fdb0f805ea4c5ff72b
sppirtti <samppa-1997@hotmail.com>
2020-04-10 17:27:17 +0300

    uibuild

---
+++ build/precache-manifest.44d3ccd94b8eb92b2884cdaf8c74c6ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1bba852229b54f4871806ef506ead6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e12f66963a05fb3a39",
+    "url": "/static/css/main.cbbb9da4.chunk.css"
+  },
+  {
+    "revision": "08d3aad3ecae6194104c",
+    "url": "/static/js/2.f518c3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f518c3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e12f66963a05fb3a39",
+    "url": "/static/js/main.816ce440.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
